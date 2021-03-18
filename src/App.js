150b3d2a5c739e3218c9f8fe0b021f08fb49c04f
Lucas Aho <aholucascode@gmail.com>
2021-03-17 20:36:28 -0600

    repo now serves as redirect to nextjs blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from './components/Footer/Footer.component';
-import BuildHome from './components/MainCompile/ViewBuilder.component';
-import BuildBlog from './components/BlogCompile/BlogViewBuilder.component';
-import ArticleView from './components/BlogCompile/ArticleView.component';
-import { Element } from 'react-scroll';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={BuildHome} />
-        <Route exact path="/blog/60340ddb95cb383ea4723058" component={BuildBlog} />
-        <Route exact path="/blog/:id" component={ArticleView} />
-
-      </Switch>
-
-      <Element id='contact' name='contact'>
-        <Footer />
-      </Element>
-    </Router>
+    <div onLoad={() => window.location.href = "https://lucas-asher.netlify.app/portfolio"}>
+      Loading New Site
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import App from "./App"
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './components/Theme/theme.component';
+import App from "./App";
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
       <App/>
-    </ThemeProvider>
   , document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
